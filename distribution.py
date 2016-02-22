fe5ea33168894b0733d9f7b5e96b95c965dcd207
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-02-22 14:55:34 -0500

    Updated from Brython Server: 2/22/2016 2:56:29 PM

--- distribution.py
+++ distribution.py
@@ -37,7 +37,7 @@ Notice about this example:
 * Letters that do not occur in the text are not listed in the output at all.
 """
 text=input("Please enter a string of text (the bigger the better): ")
-print('The distribution of characters in "' +string+ '" is: ')
+print('The distribution of characters in "' + text + '" is: ')
 
 import string
 abc= string.ascii_lowercase
