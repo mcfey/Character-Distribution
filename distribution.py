99a54a94f87a72891477013b31149ccff41c8efb
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-02-23 14:27:31 -0500

    Updated from Brython Server: 2/23/2016 2:28:27 PM

--- distribution.py
+++ distribution.py
@@ -41,7 +41,6 @@ print('The distribution of characters in "' + text + '" is: ')
 
 import string
 abc= string.ascii_lowercase
-numberscopy= list(range(0,26))
 numbers= list(range(0,26))
 
 print(numbers)
