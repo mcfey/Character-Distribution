e780394c735b090c7a5283367062e280e301761b
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-02-23 14:18:43 -0500

    Updated from Brython Server: 2/23/2016 2:19:39 PM

--- distribution.py
+++ distribution.py
@@ -42,13 +42,14 @@ print('The distribution of characters in "' + text + '" is: ')
 import string
 abc= string.ascii_lowercase
 numberscopy= list(range(0,26))
-numbers= string.ascii_lowercase
+numbers= list(range(1,27))
 
 
 for x in abc:
     n=text.count(x)
     print(n)
-    numbers[x]=n
+    for x in numbers:
+        numbers[x]=n
 
 print(numbers)
     
